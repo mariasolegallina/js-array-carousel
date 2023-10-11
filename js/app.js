9bfd48128794c0f359eed4883fa64dbe73ae9e5d
mariasolegallina <gallinamariasole@gmail.com>
2023-10-11 15:02:57 +0200

    updarte - for

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ console.log('array carousel')
 
 // creare un array con le immagini da inserire nella gallery
 const imagesArray = [
-    '../img/01.webp',
-    '../img/02.webp',
-    '../img/03.webp',
-    '../img/04.webp',
-    '../img/05.webp',
+    './img/01.webp',
+    './img/02.webp',
+    './img/03.webp',
+    './img/04.webp',
+    './img/05.webp',
 ]
 console.log(imagesArray)
 
@@ -16,5 +16,11 @@ console.log('elemento con classe gallery', galleryDOMElement)
 
 // creare un ciclo for per definire la "posizione" delle immagini nell'array e inserirle nell'html
 for (let i = 0; i < imagesArray.length; i++) {
-    console.log(imagesArray[i])
+    // console.log(imagesArray[i])
+    
+    // inviare al DOM le immagini nell'array
+    const image = imagesArray [i]
+    const imageToHtml = `<img src="${image}" alt="" class="gallery__img">`
+    galleryDOMElement.innerHTML = galleryDOMElement + imageToHtml
 }
+
