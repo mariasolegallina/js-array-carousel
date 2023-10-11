910b5a900554dbe6c1be3e5eecc256ee60cdf580
mariasolegallina <gallinamariasole@gmail.com>
2023-10-11 13:57:11 +0200

    add - gallery in html

---
+++ js/app.js
@@ -0,0 +1,17 @@
+console.log('array carousel')
+
+// const imagesArray = [
+//     '../img/01.webp',
+//     '../img/02.webp',
+//     '../img/03.webp',
+//     '../img/04.webp',
+//     '../img/05.webp',
+// ]
+// console.log(imagesArray)
+
+const galleryDOMElement = document.querySelector('.gallery')
+console.log(galleryDOMElement)
+
+for (let i = 0; i < imagesArray.length; i++) {
+    console.log([i])
+}
